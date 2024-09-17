b8f1dfd763ff6d2a9b7746a283ebea31516bb3ac
Yakie <Yakie@gmail.com>
2024-09-16 21:30:31 -0500

    pruebas unitarias

--- src/main/java/com/mycompany/parcial1/Principal.java
+++ src/main/java/com/mycompany/parcial1/Principal.java
@@ -5,4 +5,8 @@ public class Principal {
     public static void main(String[] args) {
         // TODO: Aquí ingresas tu código para iniciar la aplicación.
     }
+
+    public boolean Procesarpedido() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
